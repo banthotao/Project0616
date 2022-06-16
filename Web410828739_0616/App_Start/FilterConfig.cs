e76799ca857174cf94410cfd9b072f612fd01d19
s1082873 <banthotao@gmail.com>
2022-06-16 19:35:18 +0800

    add 3

---
+++ Web410828739_0616/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Web410828739_0616
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Web410828739_0616/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Web410828739_0616.MvcApplication" Language="C#" %>
